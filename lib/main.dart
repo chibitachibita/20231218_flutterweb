c0bca8fbc67d4085b1a2d249d84748e23428084e
chibitachibita <dizzy.xxx.666@gmail.com>
2023-12-17 16:21:27 +0000

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
-// test
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyMaterialApp());
+}
+
+class MyMaterialApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(f
+        appBar: AppBar(
+          title: Text('Material Design App'),
+        ),
+        body: Center(
+          child: Text('Hello, Material Design!'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // フローティングアクションボタンが押されたときの処理
+            print('Floating Action Button Pressed!');
+          },
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
